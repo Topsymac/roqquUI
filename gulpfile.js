21ba4626194dad33af3041fa078bed052a11955f
ChukwudiNebo <chukwudineboch@gmail.com>
2023-03-15 03:48:55 -0700

    made some changes to the shadows,radius and blur file

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-    return src('roqqu/**/*.scss')
-        .pipe(sass())
-        .pipe(dest('css'))
+    return src("roqqu/**/*.scss")
+      .pipe(sass())
+      .pipe(purgecss({ content: ["*.html"] }))
+      .pipe(dest("css"));
 }
 
 function watchTask() {
-    watch(['roqqu/**/*.scss'], buildStyles)
+    watch(["roqqu/**/*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask)
